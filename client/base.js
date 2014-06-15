f36ec4c4114a10b4268527d571ca71d72c02d0bf
Arnaud Drazek <adrazek@gmail.com>
2014-06-15 20:31:59 +0200

    Commit message

--- client/base.js
+++ client/base.js
@@ -16,7 +16,7 @@ BaseClient.prototype = Object.create(events.EventEmitter.prototype, {
 
 BaseClient.prototype.socket = null;
 
-BaseClient.prototype.setSocket = function(socket){this.socket=socket;console.log('setting socket');};
+BaseClient.prototype.setSocket = function(socket){this.socket=socket;console.log('setting socket');console.log(this.socket);console.log(socket);};
 BaseClient.prototype.getSocket = function(){console.log('getting socket : ');console.log(this.socket);return this.socket;};
 
 BaseClient.prototype.emitTcp = function(event, params, fn){this.getSocket().emit(event, params, fn);};
